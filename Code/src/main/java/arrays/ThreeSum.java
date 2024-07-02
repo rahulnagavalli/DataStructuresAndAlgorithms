eea1c048fd949664bd68ada29d34f2af134e3aea
rahulnagavalli <nagavallirahul2307@gmail.com>
2024-07-01 21:40:40 -0500

    7/1 - 3Sum

--- Code/src/main/java/arrays/ThreeSum.java
+++ Code/src/main/java/arrays/ThreeSum.java
@@ -2,7 +2,9 @@ package arrays;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 // Given an Integer Array nums, return all the triplets [ nums[i], nums[j], nums[k] ] such that nums[i] + nums[j] + nums[k] = 0. 
 // Order of the triplets doesn't matter.
@@ -15,24 +17,34 @@ public class ThreeSum {
 	}
 	
 	public static List<List<Integer>> doThreeSumOp(int[] nums) {
-		List<List<Integer>> list = new ArrayList<>();
+		Set<List<Integer>> set = new HashSet<>();
 		// sort the Array in increasing order.
-		int[] arr = {-4, -1, -1, 0, 1,2};
-		for (int i = 0; i< arr.length - 2; i++) {
+		Arrays.sort(nums);
+		for (int i = 0; i< nums.length; i++) {
+			System.out.println(nums[i]);
+		}
+		// -4, -1, -1, 0, 1,2
+		for (int i = 0; i< nums.length; i++) {
+			if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
 			int l = i+1;
-			int r = arr.length - 1;
-			while (l <=r) {
-				if (arr[i] + arr[l] + arr[r] ==0) {
-					list.add(Arrays.asList(arr[i], arr[l], arr[r]));
-				}
-				if (arr[l] + arr[r] < - arr[i]) {
+			int r = nums.length - 1;
+			while (l <r) {
+				if (nums[i] + nums[l] + nums[r] ==0) {
+					set.add(Arrays.asList(nums[i], nums[l], nums[r]));
+					l++;
+                    while (l < r && nums[l] == nums[l - 1]) {
+                        l++;
+                    }
+				} else if (nums[l] + nums[r] < - nums[i]) {
 					l = l +1;
-				} else if (arr[l] + arr[r] > - arr[i]) {
+				} else if (nums[l] + nums[r] > - nums[i]) {
 					r = r-1;
 				}
 			}
 		}
-		return null;
+		return new ArrayList<>(set);
 	}
 
 }
